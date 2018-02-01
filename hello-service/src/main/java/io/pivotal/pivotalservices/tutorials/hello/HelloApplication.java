6472fe343a26b765ed6bd2ff69a6c8c9c836839c
Matthew Campbell <mjc4648@gmail.com>
2018-01-31 21:49:04 -0500

    hello service returns OK status

--- hello-service/src/main/java/io/pivotal/pivotalservices/tutorials/hello/HelloApplication.java
+++ hello-service/src/main/java/io/pivotal/pivotalservices/tutorials/hello/HelloApplication.java
@@ -2,6 +2,8 @@ package io.pivotal.pivotalservices.tutorials.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloApplication {
@@ -9,4 +11,15 @@ public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
+
+	@RestController
+	public class HelloApi {
+
+		@GetMapping("/hello")
+		public String getHello() {
+			return null;
+		}
+
+	}
+
 }
--- hello-service/src/test/java/io/pivotal/pivotalservices/tutorials/hello/HelloApplicationTests.java
+++ hello-service/src/test/java/io/pivotal/pivotalservices/tutorials/hello/HelloApplicationTests.java
@@ -2,15 +2,36 @@ package io.pivotal.pivotalservices.tutorials.hello;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class HelloApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testHelloService_returnsGreeting() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON);
+		mockMvc.perform(request)
+				.andExpect(status().isOk());
 	}
 
 }
