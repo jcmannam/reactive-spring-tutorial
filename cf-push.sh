66673c3d6beb0354e613e98a15762ead5fc78ffa
Matthew Campbell <mjc4648@gmail.com>
2018-02-01 14:22:42 -0500

    enabled all apps in cf-push script

--- cf-push.sh
+++ cf-push.sh
@@ -1,4 +1,4 @@
-#cf push reactive-spring-zipkin-server -p zipkin-server/target/zipkin-server-0.0.1-SNAPSHOT.jar -b java_buildpack
+cf push reactive-spring-zipkin-server -p zipkin-server/target/zipkin-server-0.0.1-SNAPSHOT.jar -b java_buildpack
 cf push nonreactive-spring-name-service -p name-service/target/name-service-0.0.1-SNAPSHOT.jar -b java_buildpack -m 2G -i 2
 cf push nonreactive-spring-hello-service -p hello-service/target/hello-service-0.0.1-SNAPSHOT.jar -b java_buildpack -m 2G -i 2
 cf push reactive-spring-name-service -p name-service-reactive/target/name-service-reactive-0.0.1-SNAPSHOT.jar -b java_buildpack -m 2G -i 2
